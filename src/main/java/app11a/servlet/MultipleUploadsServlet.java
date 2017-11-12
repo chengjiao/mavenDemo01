558afa2c09020e5453efd9567fb4ad76bb069d73
chengjiao <chengjiaosky@gmail.com>
2017-11-12 16:22:26 +0800

    Merge branch 'master' of https://github.com/chengjiao/mavenDemo01
    # Conflicts:
    #	src/main/webapp/WEB-INF/web.xml

---
+++ src/main/java/app11a/servlet/MultipleUploadsServlet.java
@@ -0,0 +1,14 @@
+package app11a.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by CJ on 2017/11/12.
+ */
+
+@WebServlet(urlPatterns = {"/multipleUploads"})
+public class MultipleUploadsServlet extends HttpServlet {
+    private static final long serialVersionUID = 9991L;
+
+}
